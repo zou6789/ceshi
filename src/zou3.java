d3bae5ce20d7ec7af24bad4426716e1913583a0f
zou <438912197@qq.com>
2022-05-09 20:53:56 +0800

    zou4

--- src/zou3.java
+++ src/zou3.java
@@ -2,7 +2,7 @@ public class zou3 {
     public static void main(String[] args) {
         int x=0;
         int y=0;
-        while(x<5){
+        while(x<6){
             y=y+x;
             System.out.print(x+" "+y+" ");
             x=x+1;
